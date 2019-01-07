b397517c95fabfe86119dcbb463496a3158efe27
Kalela <philipkalela@gmail.com>
2019-01-07 15:04:12 +0300

    initial commit

---
+++ src/main/java/com/kalela/newstock/NewstockApplication.java
@@ -0,0 +1,14 @@
+package com.kalela.newstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewstockApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/kalela/newstock/NewstockApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kalela.newstock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewstockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
