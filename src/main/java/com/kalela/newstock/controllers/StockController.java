dfa738305a121a1d33fb710020f056e93fc8cf92
Kalela <philipkalela@gmail.com>
2019-01-09 22:31:48 +0300

    user can add items to stock

--- src/main/java/com/kalela/newstock/controllers/StockController.java
+++ src/main/java/com/kalela/newstock/controllers/StockController.java
@@ -3,9 +3,8 @@ package com.kalela.newstock.controllers;
 import com.kalela.newstock.models.Stock;
 import com.kalela.newstock.repositories.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,12 @@ public class StockController {
     @GetMapping
     public List<Stock> allStock() {
         return stockRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String addUser(@RequestBody Stock stock) {
+        stockRepository.save(stock);
+        return stock.getItemName() + " added to Inventory.";
     }
 }
